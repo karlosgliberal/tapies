64a7b12d5e236e0a360a8ffbac8bef6d21452c2a
Karlos g liberal (patxangas) <karlosgliberal@gmail.com>
2015-11-03 11:24:11 +0100

    Cambios con el fondo rojo

--- tachikoma/public/javascripts/site.js
+++ tachikoma/public/javascripts/site.js
@@ -6,6 +6,38 @@ $( function() {
   var estanVivos;
   window.terra;
   var width = $(window).width() / 10;
+  var criaturas = {};
+
+  criaturas.plant = {
+    type: 'plant',
+    color: [173, 173, 173],
+    size: 10,
+    initialEnergy: 5,
+    maxEnergy: 20,
+    wait: function() {
+      // photosynthesis :)
+      this.energy += 2;
+    },
+    move: false,
+    reproduceLv: 0.65
+  };
+
+  criaturas.brute = {
+    type: 'brute',
+    color: [29, 30, 25],
+    maxEnergy: 50,
+    initialEnergy: 2,
+    size: 20
+  };
+
+  criaturas.bully = {
+    type: 'bully',
+    color: [147, 112, 219],
+    initialEnergy: 20,
+    reproduceLv: 0.6,
+    sustainability: 3
+  };
+
   var Submundo = function(){
     this.terra = window.terra;
     this.bbTerrarium = new terra.Terrarium(width, 5, {id:'terra'});
@@ -30,36 +62,9 @@ $( function() {
   };
 
   var miMundo = new Submundo();
-
-  miMundo.registerCreature({
-    type: 'plant',
-    color: [173, 173, 173],
-    size: 10,
-    initialEnergy: 5,
-    maxEnergy: 20,
-    wait: function() {
-      // photosynthesis :)
-      this.energy += 2;
-    },
-    move: false,
-    reproduceLv: 0.65
-  });
-
-  miMundo.registerCreature({
-    type: 'brute',
-    color: [29, 30, 25],
-    maxEnergy: 50,
-    initialEnergy: 2,
-    size: 20
-  });
-
-  miMundo.registerCreature({
-    type: 'bully',
-    color: [147, 112, 219],
-    initialEnergy: 20,
-    reproduceLv: 0.6,
-    sustainability: 3
-  });
+  miMundo.registerCreature(criaturas.plant);
+  miMundo.registerCreature(criaturas.brute);
+  miMundo.registerCreature(criaturas.bully);
   miMundo.grid([['plant', 50], ['brute', 5], ['bully', 5]]);
   miMundo.action('animate');
 
@@ -69,7 +74,7 @@ $( function() {
   }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
-
+  $('.human').hide();
   $(document).bind('keydown', 'a', function assets() {
     loopFrases = setInterval(function(){
       $.get( "/sentencia", function( data ) {
@@ -77,7 +82,7 @@ $( function() {
           frase: data.sentences
         });
       });
-    }, 2000);
+    }, 9000);
     humanosRef.set({
       vivo:true
     });
@@ -85,7 +90,6 @@ $( function() {
   });
 
   $(document).bind('keydown', 's', function assets() {
-    miMundo.action('animate');
     clearInterval(loopFrases);
     humanosRef.set({
       vivo:false
@@ -93,20 +97,35 @@ $( function() {
   });
 
   var humanosVivos = function(frase){
+    $('canvas').remove();
+    criaturas.plant.color = [242, 50, 66]
+    criaturas.plant.type = 'sangre';
+    var humanMundo = new Submundo();
+    humanMundo.registerCreature(criaturas.plant);
+    humanMundo.registerCreature(criaturas.brute);
+    humanMundo.registerCreature(criaturas.bully);
+    humanMundo.grid([['sangre', 50], ['brute', 5], ['bully', 5]]);
+    humanMundo.action('animate');
+
     $('#sentencia').hide().html(frase.frase).fadeIn('slow');
-    $('#humanos').hide().html('<h1 style="color:red;width:50px;">Human Detect</h1>').fadeIn('slow');
+    $('.human').fadeIn();
   }
 
   var humanosMuerto = function(){
-    $(".wrap").css("border-style", "dotted");
-    $(".wrap").css("border-width", "0px");
-    $(".wrap").css("border-color", "red");
-    $('#humanos').hide().html('').fadeIn('slow');
+    $('canvas').remove();
+    criaturas.plant.color = [242, 50, 66]
+    criaturas.plant.type = 'plant';
+    var humanMundo = new Submundo();
+    humanMundo.registerCreature(criaturas.plant);
+    humanMundo.registerCreature(criaturas.brute);
+    humanMundo.registerCreature(criaturas.bully);
+    humanMundo.grid([['plant', 50], ['brute', 5], ['bully', 5]]);
+    humanMundo.action('animate');
+    $('.human').fadeOut();
   }
 
   frasesRef.on("child_added", function(snapshot, prevChildKey){
     if(estanVivos.vivo == true){
-      miMundo.action('stop');
       var frase = snapshot.val();
       humanosVivos(frase);
     }
@@ -115,7 +134,6 @@ $( function() {
   humanosRef.on("child_changed", function(snapshot, prevChildKey){
     var estado = snapshot.val();
     if(estado == false){
-      miMundo.action('animate');
       humanosMuerto();
     }
   });
